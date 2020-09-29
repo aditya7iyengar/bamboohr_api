7489df186da7a77eab6c9a8abe52a46029ea21c7
Adi Iyengar <aditya7iyengar@gmail.com>
2020-09-29 18:08:45 -0400

    Clean up

--- lib/bamboohr_api.ex
+++ lib/bamboohr_api.ex
@@ -1,16 +1,5 @@
 defmodule BamboohrApi do
-  @moduledoc """
-  Documentation for `BamboohrApi`.
-  """
+  @moduledoc false
 
-  alias __MODULE__.{Config, Entities.TimeOffRequest}
-
-  @spec request_time_off(map(), Config.t()) :: :ok | {:error, term()}
-  def request_time_off(params, config \\ nil) do
-    config = config || default_config()
-
-    TimeOffRequest.create(params, config)
-  end
-
-  defdelegate default_config, to: Config, as: :default
+  # TODO: Add better api
 end
